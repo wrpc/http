5b9a324c74d72b27d8559c54b419a54609d01c68
Roman Volosatovs <rvolosatovs@riseup.net>
2024-05-30 19:27:53 +0200

    feat(go): add examples
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

---
+++ examples/go/http-outgoing-client/http_outgoing_client.go
@@ -0,0 +1,3 @@
+//go:generate wit-bindgen-wrpc go --out-dir bindings --package github.com/wrpc/http/examples/go/http-outgoing-client/bindings wit
+
+package http_outgoing_client
---
+++ examples/go/http-outgoing-server/http_outgoing_server.go
@@ -0,0 +1,3 @@
+//go:generate wit-bindgen-wrpc go --out-dir bindings --package github.com/wrpc/http/examples/go/http-outgoing-server/bindings wit
+
+package http_outgoing_server
